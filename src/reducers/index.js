43042044dcc2767c7f5d724f76418bcbdac47b65
Omer Yildiz <yildiom@cronos.be>
2019-08-22 17:00:00 +0200

    Posts reducer with test

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
 
-import success from './successReducer';
+import posts from './posts/reducer';
 
-export default combineReducers({ success })
+export default combineReducers({ posts })
